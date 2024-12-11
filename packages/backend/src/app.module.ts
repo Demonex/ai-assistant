badc712a42e805231d6d6b569c837ae2d5bd87ab
bardak <hello@bardak.io>
2024-12-11 14:51:45 +0000

    .

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -19,8 +19,7 @@ import { ScheduleModule } from "@nestjs/schedule";
 			url: "redis://localhost:6379",
 		}),
 		ScheduleModule.forRoot(),
-
-		...Object.values(modules)
+		...Object.values(modules),
 	],
 })
 export class AppModule implements NestModule, OnApplicationShutdown {
