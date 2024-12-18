a4618eefe99a3e43d826ca1f2f581f3639efd050
diskandarov <diskandarov@sigma-it.ru>
2024-12-18 16:49:05 +0300

    Fix

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -8,21 +8,21 @@ import { Logger } from "@repo/backend/config/logger/api-logger.js";
 import * as modules from "@repo/backend/modules.exported.js";
 import { TypegooseModule } from "nestjs-typegoose";
 import { RedisModule } from "@nestjs-modules/ioredis";
-import { MONGO_CONFIG, MONGO_URI } from "./mongoose.config.js";
+import { MONGO_CONFIG, MONGO_URI } from "@repo/backend/mongoose.config.js";
 import { ScheduleModule } from "@nestjs/schedule";
 import { TypeOrmModule } from "@nestjs/typeorm";
-import { UserEntityPG } from "./entities/User/index-pg";
-import { UserRolesPG } from "./entities/User/roles-pg";
+import { UserEntityPG } from "@repo/backend/entities/User/index-pg.js";
+import { UserRolesPG } from "@repo/backend/entities/User/roles-pg.js";
 
 @Module({
 	imports: [
 		TypeOrmModule.forRoot({
 			type: "postgres",
-			host: "localhost",
-			port: 5432,
-			username: "root",
-			password: "root123",
-			database: "sigma-chat",
+			host: process.env.DATABASE_HOST,
+			port: Number.parseInt(process.env.DATABASE_PORT),
+			username: process.env.DATABASE_USERNAME,
+			password: process.env.DATABASE_PASSWORD,
+			database: process.env.DATABASE_NAME,
 			entities: [UserEntityPG, UserRolesPG],
 			synchronize: false,
 		}),
