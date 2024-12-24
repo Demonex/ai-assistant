67745c150d17b17597d1ca28676e54f7d32b0cb7
bardak <hello@bardak.io>
2024-12-24 10:19:15 +0000

    feat (admin): deps updated

--- apps/admin/src/app/(payload)/layout.tsx
+++ apps/admin/src/app/(payload)/layout.tsx
@@ -3,7 +3,7 @@
 import config from "@payload-config";
 import "@payloadcms/next/css";
 import type { ServerFunctionClient } from "payload";
-import { handleServerFunctions, RootLayout } from "@payloadcms/next/layouts";
+import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
 import type React from "react";
 
 import { importMap } from "./admin/importMap.js";
@@ -13,13 +13,11 @@ type Args = {
 	children: React.ReactNode;
 };
 
-const serverFunction: ServerFunctionClient = async (args) =>
-	handleServerFunctions({
-		...args,
-		config,
-		importMap,
-	});
-
+const serverFunction: ServerFunctionClient = async (args) => handleServerFunctions({
+    ...args,
+    config,
+    importMap,
+  })
 const Layout = ({ children }: Args) => (
 	<RootLayout
 		config={config}
