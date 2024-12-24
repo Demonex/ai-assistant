4f93c74b14fecf927bc3fa66619f6db6ca6ed64a
bardak <hello@bardak.io>
2024-12-24 08:03:29 +0000

    feat (admin): refactor

--- apps/admin/src/collections/collection/index.ts
+++ apps/admin/src/collections/collection/index.ts
@@ -4,60 +4,49 @@ import defaultAccess from "@/utilities/defaultAccess";
 import { neuro } from "@/collections/neuro";
 
 export const collection: CollectionConfig = {
-  slug: "collection",
-  access: defaultAccess,
-  admin: {
-    defaultColumns: [
-      "title",
-      "description",
-      "preview",
-    ],
-    useAsTitle: "title",
-  },
-  fields: [
-    {
-      name: "embedding",
-      type: "relationship",
-      relationTo: neuro.slug as 'neuro',
-      
-      required: true
-    },
-    {
-      name: "llm",
-      type: "relationship",
-      relationTo: neuro.slug as 'neuro',
-      required: true
-    },
-    {
-      name: "reranker",
-      type: "relationship",
-      relationTo: neuro.slug as 'neuro',
-      
-      required: true
-    },
-    {
-      name: "title",
-      type: "text",
-      required: true,
-      localized: true,
-    },
-    {
-      name: "description",
-      type: "text",
-      localized: true,
-    },
-    {
-      name: "settings",
-      type: "json",
-      required: true,
-    },
-  ],
-  versions: {
-    drafts: {
-      autosave: {
-        interval: 100, // We set this interval for optimal live preview
-      },
-    },
-    maxPerDoc: 50,
-  },
+	slug: "collection",
+	access: defaultAccess,
+	admin: {
+		defaultColumns: ["title", "description", "preview"],
+		useAsTitle: "title",
+	},
+	fields: [
+		{
+			name: "title",
+			type: "text",
+			required: true,
+			localized: true,
+		},
+		{
+			name: "description",
+			type: "text",
+			localized: true,
+		},
+		{
+			name: "embedding",
+			type: "relationship",
+			relationTo: neuro.slug as "neuro",
+			required: true,
+		},
+		{
+			name: "llm",
+			type: "relationship",
+			relationTo: neuro.slug as "neuro",
+			required: true,
+		},
+		{
+			name: "reranker",
+			type: "relationship",
+			relationTo: neuro.slug as "neuro",
+			required: true,
+		},
+	],
+	versions: {
+		drafts: {
+			autosave: {
+				interval: 100, // We set this interval for optimal live preview
+			},
+		},
+		maxPerDoc: 50,
+	},
 };
