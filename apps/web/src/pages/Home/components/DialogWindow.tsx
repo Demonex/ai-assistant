d87944c55a20c628367b344abe19e54dc0564bba
Johnny2late <german.work11@gmail.com>
2025-02-07 17:41:51 +0300

    fix: handle pin file & send

--- apps/web/src/pages/Home/components/DialogWindow.tsx
+++ apps/web/src/pages/Home/components/DialogWindow.tsx
@@ -94,7 +94,7 @@ export const DialogWindow = () => {
 		console.log(data);
 	};
 
-	const handleButtonClick = () => {
+	const handlePinFileButton = () => {
 		fileInputRef.current?.click();
 	};
 
@@ -693,11 +693,8 @@ export const DialogWindow = () => {
 								onKeyDown={handleKeyDown}
 								className="flex h-10 w-full rounded-md border bg-background px-3 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus:border-primary focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 border-transparent pe-32 !text-base !shadow-transsparent !ring-transparent lg:pe-56"
 							/>
-							<div
-								className=" end-4 flex items-center"
-								onClick={handleButtonClick}
-							>
-								<div className="block lg:hidden">
+							<div className="end-4 flex items-center">
+								<div className="relative ml-3">
 									<input
 										type="file"
 										ref={fileInputRef}
@@ -705,35 +702,10 @@ export const DialogWindow = () => {
 										style={{ display: "none" }}
 									/>
 									<button
-										className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-11 w-11 rounded-full p-0"
 										type="button"
-										id="radix-:ri:"
-										aria-haspopup="menu"
-										aria-expanded="false"
-										data-state="closed"
-									>
-										<svg
-											xmlns="http://www.w3.org/2000/svg"
-											width={24}
-											height={24}
-											viewBox="0 0 24 24"
-											fill="none"
-											stroke="currentColor"
-											strokeWidth={2}
-											strokeLinecap="round"
-											strokeLinejoin="round"
-											className="lucide lucide-circle-plus h-4 w-4"
-										>
-											<circle cx={12} cy={12} r={10} />
-											<path d="M8 12h8" />
-											<path d="M12 8v8" />
-										</svg>
-									</button>
-								</div>
-								<div className="hidden lg:block ml-3">
-									<button
 										className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-11 w-11 rounded-full p-0"
 										data-state="closed"
+										onClick={handlePinFileButton}
 									>
 										<svg
 											xmlns="http://www.w3.org/2000/svg"
@@ -753,6 +725,7 @@ export const DialogWindow = () => {
 								</div>
 								{!loading && (
 									<button
+										disabled={!message && !files.length}
 										type="submit"
 										className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 ms-3"
 									>
