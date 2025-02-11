bfdb842fcdc9a55672ffb4544d4f9f732864bbfb
bardak <hello@bardak.io>
2025-02-11 11:53:42 +0000

    chore (admin): field default value

--- apps/admin/src/collections/doc/index.ts
+++ apps/admin/src/collections/doc/index.ts
@@ -44,4 +44,40 @@ export const doc: CollectionConfig = {
 		maxPerDoc: 50,
 	},
 	upload: {},
+	hooks: {
+		beforeChange: [
+			async ({ data, user, operation, req: { headers, payload }, context }) => {
+				switch (operation) {
+					case "create": /*case "update":*/
+					{
+						const referer = headers.get("referer");
+						if (!referer) {
+							throw new Error("referer not found");
+						}
+						const collectionId = referer.split("/").at(-1) as string;
+						if (!referer) {
+							throw new Error("referer not found");
+						}
+						const collection = await payload.findByID({
+							collection: "collection",
+							id: collectionId,
+							draft: true,
+							user,
+						});
+						data.name = data.filename;
+						data.collection = collectionId;
+						const [provider] = collection.providers;
+						if (provider.length !== 24) {
+							data.provider = provider.id;
+						}
+						console.log("data", data);
+						console.log("collectionId", collectionId);
+						console.log("collection", collection);
+						console.log("provider", provider);
+						break;
+					}
+				}
+			},
+		],
+	},
 };
