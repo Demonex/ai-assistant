6c2c4df6681e27aa2498fcead2a7771c08b61288
Konstantin Zamulin <k.zamulin@sigma-it.ru>
2025-02-27 17:49:57 +0300

    Fix BACKEND url env var

--- apps/admin/src/services/CollectionService.ts
+++ apps/admin/src/services/CollectionService.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const baseURL = process.env.BACKEND_ENDPOINT || "http://10.199.35.49:2050";
+const baseURL = process.env.BACKEND_URL || "http://10.199.35.49:2050";
 
 const api = axios.create({
 	baseURL,
