7df522fd0bc6daf98977778a9f9c106c6dc65f87
MaxFun4k <maks-kuznecov11@mail.ru>
2025-02-27 17:41:49 +0300

    fix

--- apps/web/src/pages/Home/components/DialogWindow.tsx
+++ apps/web/src/pages/Home/components/DialogWindow.tsx
@@ -219,8 +219,8 @@ export const DialogWindow = () => {
 						<div data-radix-scroll-area-content>
 							<div>
 								<div className="flex flex-col items-start space-y-10 pb-[8rem]">
-									{messageMockData?.map((message) => (
-										// {messages?.map((message) => (
+									{/* {messageMockData?.map((message) => ( */}
+									{messages?.map((message) => (
 										<Fragment key={message.id}>
 											{message.request && (
 												<div className="max-w-screen-sm self-end">
