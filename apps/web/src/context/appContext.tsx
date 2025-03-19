70a88352723fe4cafff16505e9739c3911294534
MaxFun4k <maks-kuznecov11@mail.ru>
2025-03-19 14:16:34 +0300

    refactor: Unnecessary components have been removed

--- apps/web/src/context/appContext.tsx
+++
@@ -1,5 +0,0 @@
-import React, { useEffect, useState } from "react";
-
-import { useFetch } from "../hooks/useFetch.js";
-import { AppContextType, ProfileData } from "./types.js";
-import { BACKEND_URL } from "../constants/index.js";
