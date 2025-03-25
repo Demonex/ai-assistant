56106e7a94b65b4e7f9571038e6df3ad592bc9fa
diskandarov <diskandarov@sigma-it.ru>
2025-03-25 17:19:54 +0300

    fix (admin): style

--- apps/admin/src/access/collection.ts
+++ apps/admin/src/access/collection.ts
@@ -4,176 +4,173 @@ import type { FieldAccess } from "payload";
 import { getUserContext, getUserGroups } from "./coreAccess";
 
 export const getCollectionAccess = () => {
+	const create = async ({ req }) => {
+		const { user } = await getUserContext({ req });
 
-  const create = async ({ req }) => {
-    const { user } = await getUserContext({ req });
+		if (user?.superadmin) {
+			return true;
+		}
 
-    if (user?.superadmin) {
-      return true;
-    }
+		const { groups } = await getUserGroups({ req });
 
-    const { groups } = await getUserGroups({ req });
+		const hasAccess = groups.docs.some(
+			(doc) =>
+				doc.groupPermissions.includes(GROUP_PERMISSIONS.admin) ||
+				doc.groupPermissions.includes(GROUP_PERMISSIONS.collection),
+		);
 
-    const hasAccess = groups.docs.some(
-      (doc) =>
-        doc.groupPermissions.includes(GROUP_PERMISSIONS.admin) ||
-        doc.groupPermissions.includes(GROUP_PERMISSIONS.collection)
-    );
+		return hasAccess;
+	};
 
-    return hasAccess;
-  };
+	const read = async ({ req }) => {
+		const { user } = await getUserContext({ req });
 
-  const read = async ({ req }) => {
-    const { user } = await getUserContext({ req });
+		if (user?.superadmin) {
+			return true;
+		}
 
-    if (user?.superadmin) {
-      return true;
-    }
+		const { groups } = await getUserGroups({ req });
 
-    const { groups } = await getUserGroups({ req });
+		const hasAccess = groups.docs.some(
+			(doc) =>
+				doc.groupPermissions.includes(GROUP_PERMISSIONS.admin) ||
+				doc.groupPermissions.includes(GROUP_PERMISSIONS.collection),
+		);
 
-    const hasAccess = groups.docs.some(
-      (doc) =>
-        doc.groupPermissions.includes(GROUP_PERMISSIONS.admin) ||
-        doc.groupPermissions.includes(GROUP_PERMISSIONS.collection)
-    );
+		if (hasAccess) {
+			return true;
+		}
 
-    if (hasAccess) {
-      return true;
-    }
+		const collections = groups.docs.flatMap((doc) => {
+			return doc.collectionPermissions?.map((perm) => {
+				// console.log(perm, perm.collection);
 
-    const collections = groups.docs.flatMap((doc) => {
-      return doc.collectionPermissions?.map((perm) => {
-        // console.log(perm, perm.collection);
+				return perm.collection.id;
+			});
+		});
 
-        return perm.collection.id;
-      });
-    })
+		if (!collections.length) {
+			return false;
+		}
 
-    if (!collections.length) {
-      return false;
-    }
+		return {
+			id: {
+				in: collections,
+			},
+		};
+	};
 
-    return {
-      id: {
-        in: collections
-      }
-    }
-  };
+	const update = async ({ req }) => {
+		const { user } = await getUserContext({ req });
 
-  const update = async ({ req }) => {
-    const { user } = await getUserContext({ req });
+		if (user?.superadmin) {
+			return true;
+		}
 
-    if (user?.superadmin) {
-      return true;
-    }
+		const { groups } = await getUserGroups({ req });
 
-    const { groups } = await getUserGroups({ req });
+		const hasAccess = groups.docs.some(
+			(doc) =>
+				doc.groupPermissions.includes(GROUP_PERMISSIONS.admin) ||
+				doc.groupPermissions.includes(GROUP_PERMISSIONS.collection),
+		);
 
-    const hasAccess = groups.docs.some(
-      (doc) =>
-        doc.groupPermissions.includes(GROUP_PERMISSIONS.admin) ||
-        doc.groupPermissions.includes(GROUP_PERMISSIONS.collection)
-    );
+		return hasAccess;
+	};
 
-    return hasAccess;
-  };
+	const _delete = async ({ req }) => {
+		const { user } = await getUserContext({ req });
 
-  const _delete = async ({ req }) => {
-    const { user } = await getUserContext({ req });
+		if (user?.superadmin) {
+			return true;
+		}
 
-    if (user?.superadmin) {
-      return true;
-    }
+		const { groups } = await getUserGroups({ req });
 
-    const { groups } = await getUserGroups({ req });
+		const hasAccess = groups.docs.some(
+			(doc) =>
+				doc.groupPermissions.includes(GROUP_PERMISSIONS.admin) ||
+				doc.groupPermissions.includes(GROUP_PERMISSIONS.collection),
+		);
 
-    const hasAccess = groups.docs.some(
-      (doc) =>
-        doc.groupPermissions.includes(GROUP_PERMISSIONS.admin) ||
-        doc.groupPermissions.includes(GROUP_PERMISSIONS.collection)
-    );
+		return hasAccess;
+	};
 
-    return hasAccess;
-  };
-
-  return {
-    create,
-    read,
-    update,
-    delete: _delete,
-  };
+	return {
+		create,
+		read,
+		update,
+		delete: _delete,
+	};
 };
 
 export const getDropDownAccess = () => {
-    const read: FieldAccess = async ({ req, id, doc, siblingData }) => {
+	const read: FieldAccess = async ({ req, id, doc, siblingData }) => {
+		const { user } = await getUserContext({ req });
 
-      const { user } = await getUserContext({ req });
+		if (user?.superadmin) {
+			return true;
+		}
 
-      if (user?.superadmin) {
-        return true;
-      }
+		const { groups } = await getUserGroups({ req });
 
-      const { groups } = await getUserGroups({ req });
+		const hasAccess = groups.docs.some(
+			(doc) =>
+				doc.groupPermissions.includes(GROUP_PERMISSIONS.admin) ||
+				doc.groupPermissions.includes(GROUP_PERMISSIONS.collection),
+		);
 
-      const hasAccess = groups.docs.some(
-        (doc) =>
-          doc.groupPermissions.includes(GROUP_PERMISSIONS.admin) ||
-          doc.groupPermissions.includes(GROUP_PERMISSIONS.collection)
-      );
+		if (hasAccess) {
+			return true;
+		}
 
-      if (hasAccess) {
-        return true;
-      }
+		const access = groups.docs.some((group) => {
+			return group.collectionPermissions?.some((perm) => {
+				// console.log(perm, perm.collection);
 
-      const access = groups.docs.some((group) => {
-        return group.collectionPermissions?.some((perm) => {
-          // console.log(perm, perm.collection);
+				return perm.collection.id === doc?.id && perm.permissions.includes("w");
+			});
+		});
 
-          return perm.collection.id === doc?.id && perm.permissions.includes('w');
-        });
-      })
+		// console.log(collections, id, doc, collections.includes(Number(id)), "CHECK");
 
-      // console.log(collections, id, doc, collections.includes(Number(id)), "CHECK");
+		return access;
+	};
 
+	const update: FieldAccess = async ({ req, id, data, doc, siblingData }) => {
+		const { user } = await getUserContext({ req });
 
-      return access;
-    }
+		if (user?.superadmin) {
+			return true;
+		}
 
-    const update: FieldAccess = async ({ req, id, data, doc, siblingData }) => {
-      const { user } = await getUserContext({ req });
+		const { groups } = await getUserGroups({ req });
 
-      if (user?.superadmin) {
-        return true;
-      }
+		const hasAccess = groups.docs.some(
+			(doc) =>
+				doc.groupPermissions.includes(GROUP_PERMISSIONS.admin) ||
+				doc.groupPermissions.includes(GROUP_PERMISSIONS.collection),
+		);
 
-      const { groups } = await getUserGroups({ req });
+		if (hasAccess) {
+			return true;
+		}
 
-      const hasAccess = groups.docs.some(
-        (doc) =>
-          doc.groupPermissions.includes(GROUP_PERMISSIONS.admin) ||
-          doc.groupPermissions.includes(GROUP_PERMISSIONS.collection)
-      );
+		const access = groups.docs.some((group) => {
+			return group.collectionPermissions?.some((perm) => {
+				// console.log(perm, perm.collection);
 
-      if (hasAccess) {
-        return true;
-      }
+				return perm.collection.id === doc?.id && perm.permissions.includes("w");
+			});
+		});
 
-      const access = groups.docs.some((group) => {
-        return group.collectionPermissions?.some((perm) => {
-          // console.log(perm, perm.collection);
+		// console.log(collections, id, doc, collections.includes(Number(id)), "CHECK");
 
-          return perm.collection.id === doc?.id && perm.permissions.includes('w');
-        });
-      })
+		return access;
+	};
 
-      // console.log(collections, id, doc, collections.includes(Number(id)), "CHECK");
-
-      return access;
-    }
-
-    return {
-      read,
-      update
-    }
-}
+	return {
+		read,
+		update,
+	};
+};
