84c43de9db428da81354b2a009e917369cb19905
Герман Евгений Юрьевич <egerman@sigma-it.ru>
2025-04-10 15:05:39 +0300

    fix (web): validate textaria

--- apps/web/src/components/Chat/DialogWindow/component/ChatForm.tsx
+++ apps/web/src/components/Chat/DialogWindow/component/ChatForm.tsx
@@ -191,7 +191,7 @@ export const ChatForm = memo<ChatInputProps>(
 					</div>
 					<button
 						type="submit"
-						disabled={!message && !files?.length}
+						disabled={!message.trim() && !files?.length}
 						className="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-11 w-11 rounded-full p-0 ms-3"
 					>
 						{messageLoading || fileLoading ? (
